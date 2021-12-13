8597ffdd496de93d5424f47aff504c6c8b47fbd6
Garnik Khachatryan <garniktester@gmail.com>
2021-12-14 02:34:34 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int costumer_account = 2_000_000_000;
+        int money_transfer = 500_000_000;
+        int total = costumer_account + money_transfer;
+        System.out.println(total);
+    }
+}
